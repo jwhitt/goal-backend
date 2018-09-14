505a16a78fe11e1078a571af8ca8a52414123c94
jw34036 <jw34036@gmail.com>
2018-09-14 00:33:20 -0500

    removed unused classes. changed port to 8090

--- src/main/kotlin/com/edwardjones/codefest/goalbackend/model/CalcOutput.kt
+++
@@ -1,4 +0,0 @@
-package com.edwardjones.codefest.goalbackend.model
-
-data class CalcOutput(val before: GoalResultContainer? = null, val after: GoalResultContainer? = null) {
-}
--- src/main/kotlin/com/edwardjones/codefest/goalbackend/model/GoalResultContainer.kt
+++
@@ -1,5 +0,0 @@
-package com.edwardjones.codefest.goalbackend.model
-
-data class GoalResultContainer(val overallSuccessRate: Float, val goalResultList: List<String>) {
-
-}
