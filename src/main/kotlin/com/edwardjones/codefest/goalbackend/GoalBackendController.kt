70ea2061b93adf757bd1a55e4f1f2b735b924ad7
jw34036 <jw34036@gmail.com>
2018-09-13 23:13:23 -0500

    integrated codefest-calcs; fixes for build

--- src/main/kotlin/com/edwardjones/codefest/goalbackend/GoalBackendController.kt
+++ src/main/kotlin/com/edwardjones/codefest/goalbackend/GoalBackendController.kt
@@ -1,18 +1,21 @@
 package com.edwardjones.codefest.goalbackend
 
-import com.edwardjones.codefest.goalbackend.model.CalcOutput
+import com.edwardjones.codefest.goalbackend.model.CalcInput
+import com.edwardjones.financialassessment.calculators.contributionanalyzer.ContributionAnalysisSummary
+import com.edwardjones.financialassessment.calculators.contributionanalyzer.ContributionAnalzyer
+
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
 class GoalBackendController {
 
-    @PostMapping("/calc")
-    fun calculateGoalProjection() : Mono<CalcOutput> {
-        // TODO call Scott's code here
 
-        var result = CalcOutput(null,null)
+    @PostMapping("/calc")
+    fun calculateGoalProjection(@RequestBody input : CalcInput) : Mono<ContributionAnalysisSummary> {
+        var result = ContributionAnalzyer().analyzeContributionAmt(input.amount.toDouble())
         return Mono.just(result)
     }
 }
